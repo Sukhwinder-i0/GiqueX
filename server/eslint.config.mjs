7069f056ae5a7e4228cf9a6cc884c5d253c1d5af
Sukhwinder-i0 <sukhwindersingh40339@gmail.com>
2025-06-30 19:46:37 +0530

    init: setup project, linting and formatting scripts

---
+++ server/eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
---
+++ server/src/test.ts
@@ -0,0 +1,7 @@
+const x = 5;
+
+const sayHello = (name: any) => {
+  console.log('Hello ' + name);
+};
+
+sayHello('Sukhwinder');
