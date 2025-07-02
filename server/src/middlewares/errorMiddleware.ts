0dc022cd8336eb40446a02d43181e7e8f4b7a20e
Sukhwinder-i0 <sukhwindersingh40339@gmail.com>
2025-07-02 21:25:01 +0530

    typo

--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { ApiError } from '../utils/ApiError';
+import ApiError from '../utils/ApiError';
 
 export const errorMiddleware = (
   err: ApiError,
